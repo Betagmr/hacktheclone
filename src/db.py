412d3a5cee174449e7ab86e94267b852a7378084
Alex <aeganarte@gmail.com>
2023-04-27 21:06:49 +0200

    ADD: database classes

--- src/db.py
+++ src/db.py
@@ -1,26 +1,25 @@
 from typing import List
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import backref, relationship, sessionmaker
+from sqlalchemy.orm import backref, relationship, sessionmaker, Session, DeclarativeBase
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from pathlib import Path
 
-Base = declarative_base()
-engine = create_engine("sqlite://", echo=True)
-Session = sessionmaker(bind=engine)
+class Base(DeclarativeBase): 
+    pass
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "user"
 
     id = Column(Integer, primary_key=True)
-    username: Column(String(30))
-    password: Column(String(30))
+    username = Column(String(30))
+    password = Column(String(30))
 
     machines = relationship("Machine", backref="user", cascade="all, delete-orphan")
 
-    def __str__(self):
+    def __repr__(self):
         return self.username
     
 class Machine(Base):
-    __tablename__ = "machines"
+    __tablename__ = "machine"
     id = Column(Integer, primary_key=True)
     root = Column(String(50))
     status = Column(String(10))
@@ -32,57 +31,68 @@ class Machine(Base):
     
 
 class Image(Base):
-    __tablename__ = "images"
+    __tablename__ = "image"
     id = Column(Integer, primary_key=True)
     machines = relationship("Machine", backref="image", cascade="all, delete-orphan")
 
 
-def get_user(user_id):
-    session = Session()
-    user = session.query(User).filter(User.id == user_id).first()
-    session.close()
-    return user
+class DBManager():
+
+    def __init__(self):
+        self.db_path = Path().cwd() / 'database.db'
+        self.engine = create_engine(rf"sqlite:///{self.db_path}", echo=True)
 
 
-def create_user(username, password):
-    session = Session()
-    user = User(username=username, password=password)
-    session.add(user)
-    session.commit()
-    session.close()
-    return user
+    def get_user(self, user_id: int) -> User:
+        with Session(self.engine) as session:
+            user = session.query(User).filter(User.id == user_id).first()
+            return user
+        
+
+    def create_user(self, username: str, password: str) -> User:
+        with Session(self.engine) as session:
+            user = User(username=username, password=password)
+            session.add(user)
+            session.commit()
+            session.expunge(user)
+            return user
 
 
-def delete_machine(machine_id):
-    session = Session()
-    machine = session.query(Machine).filter(machine.id == machine_id).first()
-    if machine:
-        session.delete(machine)
-        session.commit()
-    session.close()
+    def delete_machine(self, machine_id: int) -> None | Machine:
+        with Session(self.engine) as session:
+            machine = session.query(Machine).filter(machine.id == machine_id).first()
+            if machine:
+                session.delete(machine)
+                session.commit()
+                return machine
+            
+            return None
 
 
-def add_machine(user_id, root, image_id):
-    session = Session()
-    user = session.query(User).filter(User.id == user_id).first()
-    image = session.query(Image).filter(Image.id == image_id).first()
-    if user and image:
-        machine = Machine(user_id = user_id, root=root, status="Sin iniciar", image_id=image_id)
-        user.machines.append(machine)
-        image.machines.append(machine)
-        session.commit()
-    session.close
+
+    def add_machine(self, user_id: int, root: str, image_id: int) -> None | Machine:
+        with Session(self.engine) as session:
+            user = session.query(User).filter(User.id == user_id).first()
+            image = session.query(Image).filter(Image.id == image_id).first()
+            
+            if user and image:
+                machine = Machine(user_id = user_id, root=root, status="Sin iniciar", image_id=image_id)
+                user.machines.append(machine)
+                image.machines.append(machine)
+                session.commit()
+                return machine
+            
+            return None
 
 
-def update_status(machine_id, status):
-    session = Session()
-    machine = session.query(Machine).filter(machine.id == machine_id).first()
-    if machine:
-        machine.status = status
-        session.commit()
-    session.close()
+    def update_status(self, machine_id: int, status: str) -> None | Machine:
+        with Session(self.engine) as session:
+            machine = session.query(Machine).filter(machine.id == machine_id).first()
+            if machine:
+                machine.status = status
+                session.commit()
+                return machine
+            
+            return None
+        
 
-
-if __name__ == '__main__':
-    Base.metadata.drop_all(engine)
-    Base.metadata.create_all(engine)
