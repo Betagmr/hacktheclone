e0e62a01109b1730158f28753608f2ec78dd3bef
Asier Villar <asiervillar@outlook.es>
2023-05-12 02:23:51 +0200

    ADD: star, check_flag, stop functionalties

--- src/app.py
+++ src/app.py
@@ -3,60 +3,112 @@ import streamlit as st
 from config import Page
 from db import DBManager
 from login import login
+from machine_controller import MachineController
 from register import register
+from utils import MachineInfo, get_machines_info
 
 
-def app() -> None:
-    username = st.session_state["username"]
-    db = st.session_state["database"]
+def render_header(username: str) -> None:
     st.title("Hack The Clone :rocket:")
 
     name_col, logout_col = st.columns([7.5, 1])
-
     with name_col:
         st.write(f"Welcome {username}!")
     with logout_col:
         logoutbutton = st.button("Logout")
-
-    selectBox = st.selectbox(
-        "Máquinas disponibles", ["Máquina 1", "Máquina 2", "Máquina 3"]
-    )
-
-    col1, col2 = st.columns([1, 1])
-
-    with col1:
-        st.header("Máquina 1")
-        st.write(f"Dificultad: {':star:' * 5}")
-        st.write("Estado: :red_circle:")
-
-        cont_col1, cont_col2, cont_col3 = st.columns([1, 1, 1])
-        with cont_col1:
-            start_button = st.button("Start", use_container_width=True)
-        with cont_col2:
-            resume_button = st.button("Stop", use_container_width=True)
-        with cont_col3:
-            start_button = st.button("Reset", use_container_width=True)
-
-        st.write("")
-
-        root_field = st.text_input("Flag", placeholder="Insert flag here...")
-        submit = st.button("Submit", use_container_width=True)
-
-    with col2:
-        st.write("")
-        st.image("./assets/placeholder.png", use_column_width=True)
 
     if logoutbutton:
         st.session_state["islogged"] = Page.LOGIN
         st.experimental_rerun()
 
 
+def render_machine_info(
+    selected_machine: MachineInfo,
+    state: str,
+    machine_ip: str,
+    mc_controller: MachineController,
+) -> tuple[str, bool]:
+    st.header(selected_machine.display_name)
+    st.write(f"Difficulty: {':star:' * 5}")
+    st.write(f"State: {state}")
+
+    cont_col1, cont_col2, cont_col3 = st.columns([1, 1, 1])
+    with cont_col1:
+        start_button = st.button("Start", use_container_width=True)
+        if start_button:
+            if not machine_ip:
+                image = mc_controller.build_image(selected_machine.container_path)
+                container = mc_controller.run_machine(image)
+                st.session_state["machine_ip"] = mc_controller.get_container_ip(container)
+                st.experimental_rerun()
+            else:
+                st.error("Machine already running - Stop it first")
+
+    with cont_col2:
+        stop_button = st.button("Stop", use_container_width=True)
+        if stop_button:
+            mc_controller.stop_running_machine()
+            st.session_state["machine_ip"] = None
+            st.experimental_rerun()
+
+    with cont_col3:
+        start_button = st.button("Reset", use_container_width=True)
+
+    st.write("")
+
+    root_field = st.text_input("Flag", placeholder="Insert flag here...")
+    submit = st.button("Submit", use_container_width=True)
+
+    return root_field, submit
+
+
+def app() -> None:
+    username = st.session_state["username"]
+    db = st.session_state["database"]
+    mc_controller = st.session_state["machine_controller"]
+    machile_list = st.session_state["machine_list"]
+    machine_ip = st.session_state["machine_ip"]
+    state = "🟢" if machine_ip else "🔴"
+
+    render_header(username)
+
+    select_box = st.selectbox(
+        "Available machines",
+        sorted([machine.container_name for machine in machile_list]),
+    )
+    selected_machine = next(
+        machine for machine in machile_list if machine.container_name == select_box
+    )
+
+    col1, col2 = st.columns([1, 1])
+    with col1:
+        root_field, submit = render_machine_info(
+            selected_machine,
+            state,
+            machine_ip,
+            mc_controller,
+        )
+    with col2:
+        st.write("")
+        st.image("./assets/placeholder.png", use_column_width=True)
+
+    if machine_ip:
+        st.info(f"Machine IP: {machine_ip}", icon="🤖")
+
+    if submit:
+        if root_field == selected_machine.flag:
+            st.success("Flag is correct!")
+        else:
+            st.error("Flag is incorrect!")
+
+
 if __name__ == "__main__":
     if "islogged" not in st.session_state:
         st.session_state["islogged"] = Page.LOGIN
-
-    if "database" not in st.session_state:
         st.session_state["database"] = DBManager()
+        st.session_state["machine_list"] = get_machines_info()
+        st.session_state["machine_controller"] = MachineController()
+        st.session_state["machine_ip"] = None
 
     if st.session_state["islogged"] == Page.APP:
         app()
--- src/utils.py
+++ src/utils.py
@@ -1,21 +1,27 @@
-from pathlib import Path
 from dataclasses import dataclass
+from pathlib import Path
+
+from config import MACHINE_FOLDER
 
 
 @dataclass
 class MachineInfo:
     container_name: str
+    container_path: Path
     flag: str
     image: Path
+
+    @property
+    def display_name(self) -> str:
+        return self.container_name.replace("_", " ").title()
 
 
 def get_machines_info() -> list[MachineInfo]:
     machines = []
 
-    folder_path = Path().cwd() / "containers"
-    for machine_folder in folder_path.iterdir():
+    for machine_folder in MACHINE_FOLDER.iterdir():
         container = machine_folder.name
-        container_path = folder_path / container
+        container_path = MACHINE_FOLDER / container
 
         image = container_path / "image.png"
 
@@ -23,7 +29,7 @@ def get_machines_info() -> list[MachineInfo]:
         with open(flag_path, "r") as file:
             flag = file.readlines()[0].replace("\n", "")
 
-        machine = MachineInfo(container, flag, image)
+        machine = MachineInfo(container, container_path, flag, image)
         machines.append(machine)
 
     return machines
